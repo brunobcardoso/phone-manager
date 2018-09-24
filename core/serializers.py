6e9d096bae385e685c4e31dba787a234f5e5a6d4
Bruno <cardosobrunob@gmail.com>
2018-09-24 20:19:28 -0300

    Create EndRecordSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -8,6 +8,19 @@ class CallSerializer(serializers.ModelSerializer):
     source = serializers.CharField(required=True, validators=[Call.phone_validator])
     destination = serializers.CharField(required=True, validators=[Call.phone_validator])
 
+    def validate(self, attrs):
+        call_data = {
+            'id': attrs.get('call_id'),
+            'source': attrs.get('source'),
+            'destination': attrs.get('destination')
+        }
+
+        call = Call(**call_data)
+        call.validate_source_destination()
+        call.full_clean()
+
+        return attrs
+
 
 class StartRecordSerializer(CallSerializer):
     class Meta:
@@ -22,6 +35,33 @@ class StartRecordSerializer(CallSerializer):
         }
 
         call = Call.objects.create(**call_data)
+
+        record_data = {
+            'call': call,
+            'type': validated_data.get('type'),
+            'timestamp': validated_data.get('timestamp')
+        }
+
+        Record.objects.create(**record_data)
+
+        return validated_data
+
+
+class EndRecordSerializer(serializers.ModelSerializer):
+    call_id = serializers.IntegerField(required=True)
+
+    class Meta:
+        model = Record
+        exclude = ('call',)
+
+    def validate(self, attrs):
+        record = Record(**attrs)
+        record.full_clean()
+
+        return attrs
+
+    def create(self, validated_data):
+        call = Call.objects.get(id=validated_data.get('call_id'))
 
         record_data = {
             'call': call,

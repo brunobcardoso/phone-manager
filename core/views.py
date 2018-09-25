7fec4b02c4dc2d42a1c03d6b05f8cb86db24ba18
Bruno <cardosobrunob@gmail.com>
2018-09-25 08:42:13 -0300

    Create Records endpoint

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
-# from django.shortcuts import render
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from core.serializers import StartRecordSerializer, EndRecordSerializer
+
+
+class RecordViewSet(viewsets.ViewSet):
+    """
+    API endpoint that allow records to be created
+
+    create:
+    Create a start or end call record
+
+    """
+    def create(self, request):
+        if request.data.get('type') == 'start':
+            serializer = StartRecordSerializer(data=request.data)
+        else:
+            serializer = EndRecordSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

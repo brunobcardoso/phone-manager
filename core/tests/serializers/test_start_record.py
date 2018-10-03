74fd3fede2c46582f4503f6deda790ada24b3778
Bruno <cardosobrunob@gmail.com>
2018-10-03 02:09:05 -0300

    Fix phone number validator
    A mobile number must start with the 9(nine) digit
    
    Add migration source and destination validators

--- core/tests/serializers/test_start_record.py
+++ core/tests/serializers/test_start_record.py
@@ -10,7 +10,7 @@ def test_record_is_valid():
         'call_id': '42',
         'timestamp': timezone.now().isoformat(),
         'source': '99988526423',
-        'destination': '9993468278'
+        'destination': '9933468278'
     }
     serializer = StartRecordSerializer(data=data)
 
@@ -23,7 +23,7 @@ def test_create():
         'call_id': '42',
         'timestamp': timezone.now().isoformat(),
         'source': '99988526423',
-        'destination': '9993468278'
+        'destination': '9933468278'
     }
     serializer = StartRecordSerializer(data=data)
     serializer.is_valid()
